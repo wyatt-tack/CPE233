c3419bf9b90c0088bd5718cb58f2a2232ef6e70a
Wyatt Tack <wtack@calpoly.edu>
2024-03-12 12:41:02 -0700

    I GET THOSE GOOSEBUMPS EVRY TIME YUH I NEED DA HIMLEK

--- Accelerometer_Test/Accelerometer_Test.ino
+++ Accelerometer_Test/Accelerometer_Test.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+
+
+#include <Wire.h>
+
+int16_t AccX, AccY;
+
+
+void gyro_signals(void) {
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x68,2); //4 to read from Y
+//  int16_t AccXLSB = Wire.read() << 8 | Wire.read();
+//  int16_t AccYLSB = Wire.read() << 8 | Wire.read();
+ AccX = Wire.read() << 8 | Wire.read();
+ //AccY = Wire.read() << 8 | Wire.read();
+
+
+//  AccX=(float)AccX*90/4096;
+//  AccY=(float)AccYLSB/4096;
 
 }
+void setup() {
+  Serial.begin(57600);
 
+  Wire.setClock(400000);
+  Wire.begin();
+
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  gyro_signals();
+  Serial.print("X = "); 
+  Serial.println(AccX);
+  //Serial.println(AccX, HEX);
+  //Serial.print("  Y = ");
+  //Serial.println(AccY, HEX);
+  delay(50);
 }
